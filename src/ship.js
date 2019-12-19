06b917d59957613668924b7e01c5aded1d4901dc
Moses Ogwo <mosesogwo@gmail.com>
2019-12-19 17:53:04 +0100

    add ship factory to src/ship.js

---
+++ src/ship.js
@@ -0,0 +1,22 @@
+const shipFactory = (length) => {
+  const shipArray = Array(length).fill(0);
+
+  const getLength = () => length;
+  const getShipArray = () => shipArray;
+  const hit = (pos) => {
+    shipArray[pos] = 1;
+  }
+  const isSunk = () => {
+    let status = true;
+    shipArray.forEach(value => {
+      if (value === 0) {
+        status = false;
+      }
+    });
+    return status;
+  }
+
+  return { getShipArray, hit, isSunk, getLength }
+}
+
+export default shipFactory;
