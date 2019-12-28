94ea274c30df97be3b426e6f610c449de1826fa9
danielmitiku <wedanielmitiku@gmail.com>
2019-12-28 10:23:12 +0300

    added player module

---
+++ src/player.js
@@ -0,0 +1,57 @@
+const player = (name, computer=0) => {
+  const nextMove = [-1,-1];
+
+  const getName = () => name;
+
+  const isComputer = () => {
+    if (computer === 0) {
+      return false;
+    }
+    return true;
+  };
+
+  const attack = (enemyBoard, x, y) => {
+    if (enemyBoard.getBoard()[x][y] === 0) {
+      enemyBoard.receiveAttack(x,y);
+      return true;
+    } 
+    return false;
+  };
+  
+  const makeRandomMove = (enemyBoard) => {
+    let xMove, yMove; 
+    let nextXMove = nextMove[0];
+    let nextYMove = nextMove[1];
+    if (nextXMove !== -1 && nextYMove !== -1 && nextXMove < 10 && nextYMove < 10 && enemyBoard.getBoard()[nextXMove][nextYMove] === 0) {
+      xMove = nextXMove;
+      yMove = nextYMove;
+    }
+    else {
+      while (enemyBoard.getBoard()[xMove][yMove] !== 0) {
+        //xMove
+        //yMove
+      }
+    }
+    
+    let move = enemyBoard.receiveAttack(x,y);
+    if (move === true) {
+      //let randomBetweenXY
+      if (randomBetweenXY === 0) {
+        nextMove[0] = xMove;
+        nextMove[1] = yMove + 1;
+      }
+      else {
+        nextMove[0] = xMove + 1;
+        nextMove[1] = yMove;
+      }
+    } else {
+      nextMove[0] = -1;
+      nextMove[1] = -1;
+    }
+  };
+
+  return { getName, isComputer, attack, makeRandomMove }
+
+};
+
+export default player;
